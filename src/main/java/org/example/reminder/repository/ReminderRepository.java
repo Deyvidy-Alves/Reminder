6af6a0baff1bff7255d5f9fcb143372cc5470753
Deyvidy-Alves <deyvidyalvessilva@gmail.com>
2026-01-07 23:06:53 -0300

    Adicionando código repository

---
+++ src/main/java/org/example/reminder/repository/ReminderRepository.java
@@ -0,0 +1,8 @@
+package org.example.reminder.repository;
+import org.example.reminder.entity.Reminder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReminderRepository extends JpaRepository<Reminder, Long>{
+
+
+}
