ead74e92383844036b92fccc2c5a9b9eeafbb76c
Deyvidy-Alves <deyvidyalvessilva@gmail.com>
2026-01-07 23:07:15 -0300

    Adicionando código service

---
+++ src/main/java/org/example/reminder/service/ReminderService.java
@@ -0,0 +1,16 @@
+package org.example.reminder.service;
+import org.example.reminder.repository.ReminderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReminderService {
+
+    private final ReminderRepository reminderRepository;
+
+    public ReminderService(ReminderRepository reminderRepository) {
+        this.reminderRepository = reminderRepository;
+    }
+
+    public createReminder()
+
+}
